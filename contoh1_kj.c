cc465e97f6eab0134d2e7d24df8b44d2d4203430
BiggusMaximus <jahekeraton164@gmail.com>
2022-03-29 21:20:12 +0700

    contoh 1

---
+++ contoh1_kj.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define phi 3.14
+int main()
+{
+
+    float jarijari, luas, keliling;
+    printf("Masukan nilai jari - jari lingkaran: ");
+    scanf("%f", &jarijari);
+    luas = phi * (jarijari * jarijari);
+    keliling = 2 * phi * jarijari;
+    printf("\nLuas lingkaran dengan jari - jari % f adalah: % f ", jarijari, luas);
+    printf("\nKeliling lingkaran dengan jari - jari %f adalah: %f", jarijari, keliling);
+    return 0;
+}
---
+++ contoh1_soal.c
@@ -0,0 +1,18 @@
+// Program untuk menghitung Luas Area dari suatu lingkaran
+
+#include <stdio.h>
+#define phi 3.141597
+int main()
+{
+    // Isilah garis kosong dibawah dengan benar
+
+    // Deklarasikan variabel luas, jari-jari dan keliling untuk dapat membuat program bekerja
+    float __, __, __;
+    printf("Masukan nilai jari - jari lingkaran: ");
+    scanf("%f", &__);
+    __ = phi * (__ * __);
+    __ = 2 * phi * __;
+    printf("\nLuas lingkaran dengan jari - jari % f adalah: % f ", __, __);
+    printf("\nKeliling lingkaran dengan jari - jari %f adalah: %f", __, __);
+    return 0;
+}
